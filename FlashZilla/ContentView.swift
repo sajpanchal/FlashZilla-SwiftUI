c556c0ccd2b5d4fa33cc8ab424ba4e1fda8eccf2
Saj Panchal <sajpanchal@gmail.com>
2021-08-28 20:41:11 -0400

    gestures in SwiftUI.

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var offset = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                self.offset = value.translation
+            }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                }
+            }
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+        let combined = pressGesture.sequenced(before: dragGesture)
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+
     }
 }
 
