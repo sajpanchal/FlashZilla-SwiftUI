956e7ab283b88dfc1728952f1404b0087f1e586e
Saj Panchal <sajpanchal@gmail.com>
2021-08-30 12:24:59 -0400

    Added the UI to stack 10 cards on top of each other. added background Image and offsets to individual cards.

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
+    @State var cards = [Card](repeating: Card.example, count: 10)
     
     var body: some View {
-        CardView(card: Card.example)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index])
+                            .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+        
+        
     }
     
     
@@ -20,5 +36,12 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset*10.0))
     }
 }
